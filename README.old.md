1b924d320c4f0fe6ed7ab73b3eac7390bb6c8117
manishankar-377 <manishankar377@gmail.com>
2025-06-26 15:36:55 -0500

    test new things

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# movieApp
-Front-end with react
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import './App.css';
-import NavBar from "./navBar/NavBar";
+import './css/App.css';
+import NavBar from "./navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
         <NavBar />
       <h1>Welcome to React</h1>
+        <Text display="Hello" header={1}/>
+        <Text display="world" header={2}/>
     </div>
   );
 }
 
+
+function Text({display, header}: {display: string, header: number}) {
+    return (
+        <div>
+        <h2>{header}</h2>
+        <p>{display}</p>
+        </div>
+    );
+}
 export default App;
